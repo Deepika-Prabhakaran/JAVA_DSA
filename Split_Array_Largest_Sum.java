c67f3af8901c860c18b5e1526fbcf2d5513ce823
Deepika-Prabhakaran <158144669+Deepika-Prabhakaran@users.noreply.github.com>
2024-09-21 14:46:38 +0530

    Add files via upload

---
+++ Split_Array_Largest_Sum.java
@@ -0,0 +1,41 @@
+
+public class Split_Array_Largest_Sum {
+    public static void main(String args[]){
+        int[] arr={7,2,5,10,8};
+        int m=2;
+        System.out.println(splitArray(arr,m));
+    }
+    
+    public static int splitArray(int[] nums, int k) {
+        int start=0;
+        int end=0;
+        
+        for(int i=0;i<nums.length;i++){
+            start=Math.max(start, nums.length);
+            end+=nums[i];
+        }  
+        while(start<end){
+            int mid= start+(end-start)/2;
+            int sum=0,pieces=1;
+            
+            for(int num:nums){
+            if(sum+num>mid){
+                sum=num;
+                pieces++;
+            }
+            else{ 
+                sum+=num;
+                
+            }
+            }
+            if(pieces>k){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }    
+            
+        }
+        return end;
+    }    
+}
